f8999db54de844d8749cee897671675a77787cd5
Aleh Brytvich <aleh_brytvich@epam.com>
2022-07-03 19:59:42 +0000

    Added d.org parser to get credits by user, by customer, by company.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dos_dashboard
 Drupal open-source dashboard
+
+
+Usage:
+
+php index.php
+
+results CSV file will be located in /tmp/contribution_credits.csv
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require 'src/Parser.php';
+use Dosdashboard\Parser;
+
+$parser = new Parser();
+echo "\n Starting...";
+//$comments = $parser->getCommentsByUser('driverok');
+$comments_by_company = $parser->getCommentsByCompany($parser::EPAM_ID);
+$comments_by_customer = $parser->getCommentsByCustomer($parser::EPAM_ID);
+$comments = array_merge($comments_by_company, $comments_by_customer);
+$comments_credited = [];
+echo "\n Comments gathered (" . count($comments). ')';
+foreach ($comments as $comment) {
+  if (!$comment || empty($comment['node'])) {
+    continue;
+  }
+  echo '.';
+  $issue = $parser->getNode($comment['node']['id'])['list'][0];
+  $project = $parser->getNode($issue['field_project']['id'])['list'][0];
+  $user = $parser->getUser($comment['author']['id'])['list'][0];
+
+
+  if (empty($issue['field_issue_credit'])) {
+    continue;
+  }
+
+  if ($issue['field_issue_status'] !== '7' && $issue['field_issue_status'] !== '2') {
+    continue;
+  }
+
+  $is_credited = array_filter($issue['field_issue_credit'], static function($v, $k) use ($comment) {return $v['id'] === $comment['cid'];}, ARRAY_FILTER_USE_BOTH);
+  if ($is_credited) {
+    $comments_credited[$issue['nid']] = $parser->prepareResponse($comment, $issue, $project, $user);
+  }
+  $parser->writeCSV($comments_credited);
+}
+echo "\n Finish. (" . (count($comments_credited)) . ' credits)';
